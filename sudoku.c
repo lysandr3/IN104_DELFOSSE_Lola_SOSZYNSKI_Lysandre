6910307cc8822fa389cbf34a44f0880bd7eca756
lysandr3 <lysandre.soszynksi@gmail.com>
2022-03-29 17:38:43 +0200

    Troisème mefvefv

--- sudoku.c
+++ sudoku.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int** build_bijection(int** p) {
+	// &p est de taille 9
+
+	int* T[9];
+	int i = 0;
+	// Faut qu'on ait une liste des nombres déj)à tiré
+	while (i<9) {
+		T[i] = i;
+		i ++;
+	}
+
+	i = 0;
+	int j;
+	while (i<9) {
+		rand()/(9-i)
+	}
+}
+
+
 int main () {
 	int area [9][9];
 	
+	// 0, 4, 8
+	// On va construire une permutation de [1;9]; on va construire une liste de 9 elts
+	// On tire un elt au hasard entre 1 et 9-k (où k est l'itération) qui ira à la 9-k position
+
 }
