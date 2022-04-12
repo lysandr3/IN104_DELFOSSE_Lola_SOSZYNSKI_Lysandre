28d1faa63586ea9b07d924e7be2270888b7736a6
lysandr3 <lysandre.soszynksi@gmail.com>
2022-04-12 17:31:31 +0200

    Affichage de grilles, Remplissage diagonales, Mise à jour possbilités et grilles

--- sudoku.c
+++ sudoku.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 
-int** build_bijection(int** p) {
+void print_tableau (int* p, int n) {
+	int i = 0;
+
+	while (i<n) {
+		printf("%d ",p[i]);
+		i++;
+	}
+
+	printf("\n");
+
+} 
+
+void build_bijection(int* p) {
 	// &p est de taille 9
 
-	int* T[9];
+	int T[9];
 	int i = 0;
-	// Faut qu'on ait une liste des nombres déjà tiré
+
 	while (i<9) {
 		T[i] = i;
 		i ++;
@@ -16,35 +29,164 @@ int** build_bijection(int** p) {
 	i = 0;
 	int j;
 	while (i<9) {
-		j = rand()/(9-i);
-		p[i] = j;
-
-		print_tableau(T,9);
-		print_tableau(p,9);
+		j = rand()%(9-i);
+		p[i] = T[j];
 
 		while (j<8-i) {
 			T[j] = T[j+1];
 			j++;
 		}
+
+		i++;
+	}
+}
+
+void build_diagonals(int* area) {
+
+	int tableau[9];
+
+	build_bijection(tableau);
+
+	int i = 0;
+	while (i<9) {
+		area[(i/3)*9+i%3] = tableau[i]+1;
+		i++;
+	}
+
+	build_bijection(tableau);
+
+	i = 0;
+	while (i<9) {
+		area[(3+i/3)*9+i%3+3] = tableau[i]+1;
+		i++;
+	}
+
+	build_bijection(tableau);
+
+	i = 0;
+	while (i<9) {
+		area[(6+i/3)*9+i%3+6] = tableau[i]+1;
+		i++;
+	}
+
+	i = 0;
+	while (i<9) {
+		print_tableau(area+9*i,9);
+		i++;
 	}
 }
 
 
-void print_tableau (int** p, int n) {
-	int i = 0;
+void update_poss(int* area, int* possible) {
 
-	while (i<n) {
-		printf("%d;",p[i]);
+	int i = 0;
+	int j;
+	while (i<4) {
+		if (area[i] == 0) {
+
+			// On se balade sur la ligne
+			j = (i/9)*9;
+			while (j/9 == i/9) {
+				if (area[j] != 0) {
+					possible[i*10+area[j]] = 1;
+				}
+				j++;
+			}
+
+			// Sur la colonne 
+			j = i%9;
+			while(j<81) {
+				if (area[j] != 0) {
+					possible[i*10+area[j]] = 1;
+				}
+				j += 9;
+			}	
+			
+			// Sur le carré 
+			j = (i/27)*27+((i%9)%3)*3;
+			int k = 0;
+			int l = 0;
+			while (k<3) {
+				while(l<3) {
+					if (area[j+k] != 0) {
+						possible[i*10+area[j]] = 1;
+					}
+					l++;
+				}
+				l = 0;
+				k++;	
+			}
+		}
+		i++;
+	}
+}
+
+void update_grid(int* area, int* possible) {
+	int i = 0;
+	int s = 0;
+	while (i<81) {
+
+		int j = 0;
+		while (j<10) {
+			s += possible[i*10+j];
+			j++;
+		}
+
+		j = 1;
+		if (s==8) {
+			while (j<10) {
+				if (possible[i*10+j] == 0) {
+					area[i] = j;
+				}
+				j++;
+			}
+		}
+		i++;
+	}
+}
+
+void print_grid(int* area) {
+
+	int i = 0;
+	printf("\n");
+	while (i<9) {
+		print_tableau(area+9*i,9);
+		i++;
+	}
+}
+
+int main () {
+	srand(time(NULL)); // Pour avoir des grilles différentes à chaque fois
+
+	// Création de la grille
+	int* area = malloc(81*sizeof(int));
+	int i = 0;
+	while (i<81) {
+		area[i] = 0;
 		i++;
 	}
 
-} 
+	// On remplit les diagonales
+	build_diagonals(area);
 
-int main () {
-	int area [9][9];
+	// Creation liste des possible
+	int* possible = malloc(81*10*sizeof(int));
+	i = 0;
+	while (i<10*81) {
+		possible[i] = 0;
+		i++;
+	}
+
 	
-	// 0, 4, 8
-	// On va construire une permutation de [1;9]; on va construire une liste de 9 elts
-	// On tire un elt au hasard entre 1 et 9-k (où k est l'itération) qui ira à la 9-k position
+	update_poss(area,possible);
+	//print_tableau(possible+30,10);
+
+	update_grid(area,possible);
+
+	print_grid(area);
+
+	// Pour chaque case vide, on va la remplir aléatoirement parmi ses possibilités
+	// On met à jour les possibilités puis la grille et on continue jusqu'a avoir un pb i.e s=9 qq part
+	// A ce moment la, ça devient plus complexe 
 
 }
