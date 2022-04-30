5d1422f2eaaae4b54d7b1b9b61b2085251ec6c24
lysandr3 <lysandre.soszynksi@gmail.com>
2022-04-30 18:02:00 +0200

    Fini.

--- sudoku.c
+++ sudoku.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-
+// Permet d'afficher un tableau linéairement
 void print_tableau (int* p, int n) {
 	int i = 0;
 
@@ -15,37 +15,40 @@ void print_tableau (int* p, int n) {
 
 } 
 
-void build_bijection(int* p) {
-	// &p est de taille 9
+// Crée des bijections aléatoires de Sn dans Sn
+void build_bijection(int* p, int n) {
 
-	int T[9];
+	int* T = malloc(n*sizeof(int)); ;
 	int i = 0;
 
-	while (i<9) {
+	while (i<n) {
 		T[i] = i;
 		i ++;
 	}
 
 	i = 0;
 	int j;
-	while (i<9) {
-		j = rand()%(9-i);
+	while (i<n) {
+		j = rand()%(n-i);
 		p[i] = T[j];
 
-		while (j<8-i) {
+		while (j<n-1-i) {
 			T[j] = T[j+1];
 			j++;
 		}
 
 		i++;
 	}
+
+	free(T);
 }
 
+// Construit nos blocs diagonaux aléatoirement
 void build_diagonals(int* area) {
 
 	int tableau[9];
 
-	build_bijection(tableau);
+	build_bijection(tableau,9);
 
 	int i = 0;
 	while (i<9) {
@@ -53,7 +56,7 @@ void build_diagonals(int* area) {
 		i++;
 	}
 
-	build_bijection(tableau);
+	build_bijection(tableau,9);
 
 	i = 0;
 	while (i<9) {
@@ -61,34 +64,35 @@ void build_diagonals(int* area) {
 		i++;
 	}
 
-	build_bijection(tableau);
+	build_bijection(tableau,9);
 
 	i = 0;
 	while (i<9) {
 		area[(6+i/3)*9+i%3+6] = tableau[i]+1;
 		i++;
 	}
-
-	i = 0;
-	while (i<9) {
-		print_tableau(area+9*i,9);
-		i++;
-	}
 }
 
-
+// Met à jours les possibilités en chaque case
 void update_poss(int* area, int* possible) {
 
 	int i = 0;
 	int j;
-	while (i<4) {
-		if (area[i] == 0) {
+
+	while (i<10*81) {
+		possible[i] = 1;
+		i++;
+	}
+
+	i = 0;
+
+	while (i<81) {
 
 			// On se balade sur la ligne
 			j = (i/9)*9;
 			while (j/9 == i/9) {
 				if (area[j] != 0) {
-					possible[i*10+area[j]] = 1;
+					possible[i*10+area[j]] = 0;
 				}
 				j++;
 			}
@@ -97,54 +101,30 @@ void update_poss(int* area, int* possible) {
 			j = i%9;
 			while(j<81) {
 				if (area[j] != 0) {
-					possible[i*10+area[j]] = 1;
+					possible[i*10+area[j]] = 0;
 				}
 				j += 9;
 			}	
 			
 			// Sur le carré 
-			j = (i/27)*27+((i%9)%3)*3;
+			j = (i/27)*27+((i%9)/3)*3;
 			int k = 0;
 			int l = 0;
 			while (k<3) {
 				while(l<3) {
-					if (area[j+k] != 0) {
-						possible[i*10+area[j]] = 1;
+					if (area[j+k*9+l] != 0) {
+						possible[i*10+area[j+l+9*k]] = 0;
 					}
 					l++;
 				}
 				l = 0;
 				k++;	
-			}
-		}
+			}	
 		i++;
 	}
 }
 
-void update_grid(int* area, int* possible) {
-	int i = 0;
-	int s = 0;
-	while (i<81) {
-
-		int j = 0;
-		while (j<10) {
-			s += possible[i*10+j];
-			j++;
-		}
-
-		j = 1;
-		if (s==8) {
-			while (j<10) {
-				if (possible[i*10+j] == 0) {
-					area[i] = j;
-				}
-				j++;
-			}
-		}
-		i++;
-	}
-}
-
+// Affiche le sudoku
 void print_grid(int* area) {
 
 	int i = 0;
@@ -153,6 +133,111 @@ void print_grid(int* area) {
 		print_tableau(area+9*i,9);
 		i++;
 	}
+}
+
+// à un indice associe le prochain qui puisse être modifié
+int suivant(int i, int* diago) {
+	if (i == 80) {
+		return (-1);
+	}
+
+	else if (diago[i+1] == 1) {
+		suivant(i+1, diago);
+	}
+
+	else {
+		return i+1;
+	}
+}
+
+// à un indice associe le precedetn pouvant être modifié
+int precedent(int i, int* diago) {
+	if (i == 0) {
+		return (-1);
+	}
+
+	else if (diago[i-1] == 1) {
+		precedent(i-1, diago);
+	}
+
+	else {
+		return i-1;
+	}
+}
+
+int premier_possible(int i,int* area, int* possible, int* tested) {
+	int j = 1;
+	while (j<10) {
+		if (possible[i*10+j] == 1 && tested[i*10+j] == 0) {
+			return j;
+		}
+		j++;
+	}
+	return 0;
+}
+
+void remplir(int i,int* diago, int* area, int* possible, int* tested) {
+
+	update_poss(area,possible); // On met à jour 
+
+	if (i != -1) {
+
+			int j = premier_possible(i,area,possible,tested);
+
+			if (j == 0) { // i.e. pas possible
+
+				// On réinitialise
+				area[i] = 0;
+			
+				int j = 1;
+				while (j<10) {
+					tested[i*10+j] = 0;
+					j++;
+				}
+			
+				remplir(precedent(i,diago),diago,area,possible,tested);
+				
+				}
+
+			else { 
+
+				area[i] = j;
+				tested[i*10+j] = 1;
+				remplir(suivant(i,diago),diago,area,possible,tested);
+
+			}
+	}
+}
+
+void real_sudoku(int n, int* area, int* sudoku) {
+	int* bij = malloc(81*sizeof(int));
+	build_bijection(bij,81);
+
+	int i = 0;
+	while(i<81) {
+		sudoku[i] == 0; 
+		i++;
+	}
+
+	i = 0;
+	while (i<n) {
+		sudoku[bij[i]] = area[bij[i]];
+		i++;
+	}
+
+	free(bij);
+}
+
+int count_empty(int* sudoku) {
+	int i = 0;
+	int j = 0;
+	while (i<81) {
+		if (sudoku[i] == 0) {
+			j++;
+		}
+		i++;
+	}
+	return j;
 }
 
 int main () {
@@ -173,20 +258,20 @@ int main () {
 	int* possible = malloc(81*10*sizeof(int));
 	i = 0;
 	while (i<10*81) {
-		possible[i] = 0;
+		possible[i] = 1;
 		i++;
 	}
 
-	
-	update_poss(area,possible);
-	//print_tableau(possible+30,10);
+	// Nous indiquera les possibilités testées
+	int* tested = malloc(81*10*sizeof(int));
 
-	update_grid(area,possible);
+	i = 0;
+	while (i<81*10) {
+		tested[i] = 0;
+		i++;
+	}
 
-	print_grid(area);
-
-	int* tested = malloc(81*10*sizeof(int));	
-
+	// Nous indiquera si on est sur un bloc diagonal
 	int* diago = malloc(81*sizeof(int));
 
 	i = 0;
@@ -202,46 +287,52 @@ int main () {
 		i++;
 	}
 
-	i = 0;
+	remplir(3,diago,area,possible,tested);	
+
+
+	int* sudoku = malloc(81*sizeof(int));
+
+	printf("Veuillez indiquer le nombre de cases découvertes : ");
+	scanf("%d",&i);
+
+	real_sudoku(i%81,area,sudoku);
+	print_grid(sudoku);
+
 	int j = 0;
+	int k = 0;
 
-	// fonction 
+	while (count_empty(sudoku) != 0) {
+		printf("Ligne : ");
+		scanf("%d",&j);
+		printf("Colonne : ");
+		scanf("%d",&k);
+		printf("Chiffre : ");
+		scanf("%d",&i);
+		sudoku[k-1+(j-1)*9] = i;
+		print_grid(sudoku);
 
-void remplir(int i) {
-
-	if (diago[i] == 1) {
-		remplir(i+1)
 	}
 
-	else if (i<81) {
-		// Si possibilitée est vide 
-
-	}
-}
-
-int poss_empty
-
+	i = 0;
+	int c = 0; 
 	while (i<81) {
-		if (area[i] == 0) {
-			j = 0;
-			while(j<10) {
-				if 
-				if ((tested[i*10+j] == 0) and (possible[i*10+j] == 1)) {
-					area[i] = j;
-					tested[i*10+j] = 1;
-				}
-			}
+		if (sudoku[i] != area[i]) {
+			c = 1;
 		}
 		i++;
 	}
 
-	// Pour chaque case vide, on va la remplir aléatoirement parmi ses possibilités
-	// On met à jour les possibilités puis la grille et on continue jusqu'a avoir un pb i.e s=9 qq part
-	// A ce moment la, ça devient plus complexe 
+	if (c==0) {
+		printf("\nFélicitations !\n");
+	}
 
-	// Parcourir les cases vides, tableau qui indiques quelles possibilitées ont été testé, une copie de possibilities avec des 1 la où ça a été testé
+	else {
+		printf("\nC'est faux.\n");
+	}
 
-
-
-
+	free(area);
+	free(possible);
+	free(tested);
+	free(diago);
+	free(sudoku);
 }
